33c912234051e0106720c1f9e91895df7d9a1da5
dec4 <dec4@illinois.edu>
2018-10-21 15:55:40 -0500

    starting selenium integration

---
+++ src/webscraping-main.py
@@ -0,0 +1,18 @@
+###
+#
+#   FROM GOOGLE COLAB Dataset_creation.ipynb
+#   Edit according to comment at bottom of test.py
+#   TODO: change output path of csv files
+#
+###
+
+import threadscraper
+
+forum_ids = [97215, 186460, 30827, 188400]
+path = "./data"
+output_files = ['singlesentence', 'mamistruggling', 'battlingdepressionnomeds', 'relationshipproblems']
+num_posts = 100
+
+for forum_id, output_file in zip(forum_ids, output_files):
+    threadscraper.getNPosts(forum_id, num_posts).to_csv(output_file + str(num_posts) + '.csv')
+
