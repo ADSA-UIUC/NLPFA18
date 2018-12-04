d3f13074f6881912809eccdca9b43d3e2992ef4f
Shaw Kagawa <skagawa2@illinois.edu>
2018-12-03 20:36:06 -0600

    added more files to pipeline in test

---
+++ test/find_closest.py
@@ -0,0 +1,15 @@
+# finding closest people to input person
+# plot everyone as single points, find closest in terms of:
+#   centroid distance
+#   average mood(?)
+#   keywords from text
+
+class find_closest:
+    def __init__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main())
