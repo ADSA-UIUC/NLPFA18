3a128516aa855819e2406004d9d245f23989b17e
Shaw Kagawa <skagawa2@illinois.edu>
2018-12-09 17:47:43 -0600

    added header from my file for reference

--- test/split_people_danielle.py
+++ test/split_people_danielle.py
@@ -1,3 +1,37 @@
+# use map to call same function on all the different sentencelevelsentiment csv's
+# add to single database (dict?) of every person
+# find way to store it in a file so it can be accessed + appended to later
+# calculate a variance value for each person to determine how many centroids
+# will be necessary to fully capture data
+
+# inside json file??
+# people_dict = {
+#     'person1': {
+#         'posts': [
+#             {
+#                 'forum_name': "",
+#                 'time': "",
+#                 'text': "",
+#                 'mood': "",
+#                 'keyword': "",
+#                 'sentiments': {
+#                     'anger': ,
+#                     ...
+#                 }
+#             },
+#             ...
+#         ],
+#         'post_centroids': [
+#             {
+#                 'anger': ,
+#                 ...
+#             },
+#             ...
+#         ]
+#     },
+#     ...
+# }
+
 def get_data(folderpath, all_files=True):
     """
     convert data into format:
