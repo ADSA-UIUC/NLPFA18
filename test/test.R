b428db69583f4d02c42a7f9efe6332fd4b290530
Shaw Kagawa <skagawa2@illinois.edu>
2019-02-17 14:49:19 -0600

    test.R edited

--- test/test.R
+++ test/test.R
@@ -6,18 +6,22 @@ library(autoplotly)
 person_df <- read_csv("data/processed/people.csv")
 
 
-filtered_df <- person_df %>%
-        filter(username == "bulbie") %>%
-        select(`sentiments.Anger`:`sentiments.Tentative`)
+#filtered_df <- person_df %>%
+#        filter(username == "bulbie") %>%
+#        select(`sentiments.Anger`:`sentiments.Tentative`)
 
-kmeans_obj <- kmeans(filtered_df, 10, nstart=25)
+
+unique_people <- data.frame(name=c("bulbie", "Starless"))
+unique_people_df <- as_tibble(unique_people %>% mutate(username = name) %>%
+                                      left_join(person_df, by = "username"))
+
+kmeans_obj <- kmeans(unique_people_df, 10, nstart=25)
 clusters <- kmeans_obj$cluster
 centers <- kmeans_obj$centers
-people.pca <- filtered_df %>% prcomp(center=TRUE)
+people.pca <- unique_people_df %>% prcomp(center=TRUE)
 
 autoplotly(people.pca, 
          loadings = TRUE, loadings.label = TRUE,
          colour = clusters,
-         data=filtered_df)
-
+         data=unique_people_df)
 
