4e7ccce67db2e5d9baa36f80a60719faeafa580a
Shaw Kagawa <skagawa2@illinois.edu>
2019-02-17 14:57:16 -0600

    demo unique_people

--- test/test.R
+++ test/test.R
@@ -11,17 +11,20 @@ person_df <- read_csv("data/processed/people.csv")
 #        select(`sentiments.Anger`:`sentiments.Tentative`)
 
 
-unique_people <- data.frame(name=c("bulbie", "Starless"))
-unique_people_df <- as_tibble(unique_people %>% mutate(username = name) %>%
-                                      left_join(person_df, by = "username"))
+unique_people <- tibble(name=c("bulbie", "Starless", "ranger"))
+unique_people_df <- unique_people %>% 
+        mutate(username = name) %>%
+        left_join(person_df, by = "username")
 
-kmeans_obj <- kmeans(unique_people_df, 10, nstart=25)
-clusters <- kmeans_obj$cluster
-centers <- kmeans_obj$centers
-people.pca <- unique_people_df %>% prcomp(center=TRUE)
+#kmeans_obj <- kmeans(unique_people_df, 10, nstart=25)
+#clusters <- kmeans_obj$cluster
+#centers <- kmeans_obj$centers
+people.pca <- unique_people_df %>% 
+        select(`sentiments.Anger`:`sentiments.Tentative`) %>%
+        prcomp(center=TRUE)
 
 autoplotly(people.pca, 
          loadings = TRUE, loadings.label = TRUE,
-         colour = clusters,
+         colour = as.numeric(as.factor(unique_people_df %>% pull(username))),
          data=unique_people_df)
 
